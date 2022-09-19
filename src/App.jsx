d3a460d542257fa1830675c789cdb4bc19bd6342
jaheimbrown <jaheimcbrown@gmail.com>
2022-09-19 16:43:19 -0500

    Structure & Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import styles from "./style";
 
-const App = () => {
-  return <div>App</div>;
-};
+// COMPONENTS
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    {/* NAVIGATION SECTION */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    {/* HERO SECTION */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{" "}
+        <Clients /> <CTA /> <Footer />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
