58774e1e66610ae25d1f8300cda1cc3a970b42c2
jaheimbrown <jaheimcbrown@gmail.com>
2022-09-23 13:06:02 -0500

    Stats Component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import styles from "../style";
+import { stats } from "../constants";
 
-const Stats = () => {
-  return <div>Stats</div>;
-};
+const Stats = () => (
+  <section className="flex flex-wrap justify-start items-start">
+    {stats.map((stat, index) => (
+      <div key={stat.id} className={`flex flex-1 justify-start items-center `}>
+        {/* <span
+          className={` ${
+            index !== 0 ? "h-[32px] w-[3px] bg-gray-500  mr-[48px]" : ""
+          }`}
+        /> */}
+
+        <h4 className="font-poppins font-semibold text-[32px] sm:text-[40px] mr-[16px] leading-[52px] text-white">
+          {stat.value}
+        </h4>
+        <p className="font-poppins text-[20px] sm:text-[18px] leading-[26px] text-gradient ">
+          {stat.title}
+        </p>
+      </div>
+    ))}
+  </section>
+);
 
 export default Stats;
