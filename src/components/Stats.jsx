5ad4aefa543cefcd6d81d73b6f935644bdab4f8a
jaheimbrown <jaheimcbrown@gmail.com>
2022-09-23 15:09:49 -0500

    responsive changes

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,19 +2,16 @@ import styles from "../style";
 import { stats } from "../constants";
 
 const Stats = () => (
-  <section className="flex flex-wrap justify-start items-start">
+  <section className="flex flex-wrap justify-start items-start sm:mb-[20px] ">
     {stats.map((stat, index) => (
-      <div key={stat.id} className={`flex flex-1 justify-start items-center `}>
-        {/* <span
-          className={` ${
-            index !== 0 ? "h-[32px] w-[3px] bg-gray-500  mr-[48px]" : ""
-          }`}
-        /> */}
-
-        <h4 className="font-poppins font-semibold text-[32px] sm:text-[40px] mr-[16px] leading-[52px] text-white">
+      <div
+        key={stat.id}
+        className={`flex flex-1 justify-start items-center sm:mr-[16px] mr-[8px]  sm:mb-0 mb-[12px] `}
+      >
+        <h4 className="font-poppins font-semibold text-[28px] sm:text-[44px] sm:mr-[12px] mr-[16px] leading-[52px] text-white">
           {stat.value}
         </h4>
-        <p className="font-poppins text-[20px] sm:text-[18px] leading-[26px] text-gradient ">
+        <p className="font-poppins text-[16px] sm:text-[20px] leading-[26px] text-gradient ">
           {stat.title}
         </p>
       </div>
