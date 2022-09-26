14321b25734bac8bc5a94e562a4ad903ed3d2c5e
jaheimabrown <jaheimcbrown@gmail.com>
2022-09-25 20:23:51 -0500

    Clients Component styling

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return <section className="flex justify-center items-center">
+    <div className="flex flex-1 flex-row flex-wrap justify-around items-center ">
+      {clients.map(client => (
+        <div key={client.id} className="w-[192px] h-[60px] mr-5 cursor-pointer">
+          <img src={client.logo} alt="client" className="w-[192px] h-[40px]  object-contain "/>
+        </div>
+      ))}
+    </div>
+
+  </section>;
 };
 
 export default Clients;
