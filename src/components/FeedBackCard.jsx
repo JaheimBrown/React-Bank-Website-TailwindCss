4be916ff0f0473c0849c69daf4d7f4d0b546cacd
jaheimabrown <jaheimcbrown@gmail.com>
2022-09-25 19:54:44 -0500

    Testimonial Component styling

--- src/components/FeedBackCard.jsx
+++ src/components/FeedBackCard.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { quotes } from "../assets";
 
-const FeedBackCard = () => {
-  return <div>FeedBackCard</div>;
+const FeedBackCard = ({content, name, title, img}) => {
+  return <div className="flex flex-col justify-between items-start feedback-card px-10 py-10 w-[375px] h-[395px] rounded-[20px] cursor-pointer xl:mb-0 mb-10 mr-0 sm:mr-6">
+    <img src={quotes} alt="quotes" />
+    <p className="font-poppins font-normal text-[18px] leading-[32px] text-white mt-10">{content}</p>
+    <div className="flex flex-row justify-center items-center">
+        <img src={img} alt={name} className="h-[48px] w-[48px] rounded-full mr-5" />
+        <div className=" flex-1 flex flex-col ">
+          <h3 className="font-poppins font-noraml text-[20px] leading-[32px] text-white">{name}</h3>
+          <p className="font-poppins font-normal text-[16px] leading-[24px] mt-1 text-dimWhite">{title}</p>
+        </div>
+    </div>
+  </div>;
 };
 
 export default FeedBackCard;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedBackCard from "./FeedBackCard";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return <section id="clients" className={`flex flex-col justify-between w-full ${styles.paddingY} relative`}>
+    {/* Gradient */}
+    <div className="absolute -right-[50%] bottom-[20%] w-[60%] h-[60%] rounded-full z-0 blue__gradient"/>
+
+    {/* HEADING */}
+    <div className="flex-1 flex md:flex-row flex-col z-[1]">
+      <h2 className={styles.heading2}>What people are <br className="sm:block hidden" />saying about us</h2>
+      <p className="font-poppins font-normal text-[18px] leading-[32px] max-w-[470px] mt-10 text-dimWhite">Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+    </div>
+
+     {/* FeedBack Cards */}
+     <div className="flex flex-1 flex-wrap md:flex-row flex-col justify-start md:justify-between  items-start mt-10 feeback-container">
+      {feedback.map(card => (
+        <FeedBackCard key={card.id} {...card} />
+      ))}
+     </div>
+  </section>;
 };
 
 export default Testimonials;
